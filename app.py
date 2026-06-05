674f8fa270779193a46dc705e421fac104440ea5
Rankivahub <hafizamirsaifi1234@gmail.com>
2026-06-05 20:51:52 +0500

    Reorganize UI setup and main content rendering

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from theme.styles import apply_theme
 from components.logo import render_logo
 from components.modal import render_analysis_modal
 
+# UI Setup
 apply_theme()
 render_logo()
+
+# Main Content
 render_analysis_modal()
